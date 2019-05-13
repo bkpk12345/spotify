4105003d5483f19683aaf3f0e2ac57df67d7d719
mr-robot <mr-robot@localhost.localdomain>
2019-05-13 19:02:21 +0530

    some new changes

--- app.js
+++ app.js
@@ -2,16 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const login = require('./routes/login');
-const https = require('https');
-const options = {
-  hostname: 'localhost'
-};
-app.use('/api', login);
 
-const port = process.env.PORT || 9000;
-const server = https.createServer(options, (req, res) => {
-  res.writeHead(200);
-  res.end('hello world\n');
-});
-
-server.listen(8888);
+app.use('/login', login);
+app.listen(6000);
