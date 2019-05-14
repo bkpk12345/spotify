70baf7dca8d80c9fdddb8574ed04551c7db52b2b
mr-robot <mr-robot@localhost.localdomain>
2019-05-14 16:26:09 +0530

    added start to npm

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const login = require('./routes/login');
-const port = process.env.PORT || 9001;
+const port = process.env.PORT || 9000;
 app.use('/api', login);
 app.listen(port, console.log(`listening at ${port}`));
