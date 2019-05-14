41e534939593f6de906c63894bae11ecf4e444d4
mr-robot <mr-robot@localhost.localdomain>
2019-05-14 15:17:09 +0530

    make it better

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const login = require('./routes/login');
-
-app.use('/login', login);
-app.listen(6000);
+const port = process.env.PORT || 9000;
+app.use('/api', login);
+app.listen(port, console.log(`listening at ${port}`));
